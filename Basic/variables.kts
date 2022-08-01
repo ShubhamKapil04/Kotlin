fde2564faffa7e69e95bc80dd4074109774ad8d5
Shubham Kapil <Skaplesh720@gmail.com>
2022-08-01 22:11:59 +0530

    Variable in kotlin

---
+++ Basic/variables.kts
@@ -0,0 +1,27 @@
+fun main(){
+
+    val name = "JonnyB" // we can change the things like its mutable valur
+//    name = "Shubham"
+
+    var isAwesome = true
+
+    println("Is " + name + "awesome? The answer is : " + isAwesome)
+
+    var a = 6
+    var b = 3
+    println(a+b)
+
+    val doubleNum = 13.43 // 64 Bits
+    println(doubleNum)
+    val floatNum = 123.45f // 32 bits
+    println(floatNum)
+
+    val longNum = 12312312432L // 64 bits
+    println(longNum)
+    val aDouble = a.toString()
+    println(aDouble)
+    var hero: String
+    hero = "batman"
+    println(hero)
+}
+main()
