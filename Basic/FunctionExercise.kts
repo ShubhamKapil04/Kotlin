ef9a705f819774fcdb60e60abc883e2c93bd529d
Shubham Kapil <Skaplesh720@gmail.com>
2022-08-04 19:18:48 +0530

    Function and Strings

---
+++ Basic/FunctionExercise.kts
@@ -0,0 +1,41 @@
+fun main(){
+
+    val num = 12
+    val num2 = 13
+
+
+    fun luketoSay(nums : Int, nums1 : Int): Int{
+
+        val sum = nums + nums1
+
+        return sum
+    }
+
+    val canday =  luketoSay(num, num2)
+    println(canday)
+//    val sum = 13
+//
+//    val candayprice = canday + sum
+//    print(candayprice)
+
+
+
+    val string = "Hello I'm not here for your Drama"
+    val string1 = "Then tell me what you want from me ?"
+
+    fun somthingToSay(char : String, char1 : String){
+
+        println("Rachit $char.\nNeha :- $char1")
+        println(char1)
+
+
+
+    }
+
+
+    somthingToSay(string, string1)
+
+
+
+}
+main()
